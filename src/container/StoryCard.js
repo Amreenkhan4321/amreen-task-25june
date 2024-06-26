c2dad06661f3f865bc039ee3f6cbe82c43ceefa3
Amreenkhan4321 <amreenkhan4899@gmail.com>
2024-06-26 23:51:49 +0530

    commit

--- src/container/StoryCard.js
+++ src/container/StoryCard.js
@@ -1,27 +1,47 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
-const StoryCard = ({data}) => {
-  // console.log(data,"data")
-  return (
-    
-  <>
-    <div className="card">
-      <Link to={`/story/${data?._id}`}>
-    <img
-      className="card-image"
-      src={`https://ik.imagekit.io/dev24/${data?.Image[0]}`}
-      alt={data?.Title}
-    />
-    <p className="card-title">{data?.Title}</p>
-    </Link>
-    <a className="read-more-button" href="">
-      {data?.Status}
-    </a>
-  </div>
-  </>
- 
-  )
-}
+const StoryCard = ({ data }) => {
 
-export default StoryCard
+  
+  const getStatusClass = (status) => {
+    console.log(status); // For debugging purposes
+    switch (status) {
+      case "Published":
+        return "blue";
+      case "Draft":
+        return "btn-new";
+      case "In Progress":
+        return "btn-inProgress";
+      case "Completed":
+        return "btn-completed";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <>
+      <div className="card">
+        <Link to={`/story/${data?._id}`}>
+          <img
+            className="card-image"
+            src={`https://ik.imagekit.io/dev24/${data?.Image[0]}`}
+            alt={data?.Title}
+          />
+          <p className="card-title">{data?.Title}</p>
+        
+        <button
+          
+          className={`read-more-button ${getStatusClass(data?.Status)}`}
+          href=""
+        >
+          {data?.Status}
+        </button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default StoryCard;
